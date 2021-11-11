3993a771971acec6de1101982a0f4919b90f38f3
bleulenny <bleulenny@github.com>
2021-11-10 21:22:54 -0800

    Added requirements txt

--- app.py
+++ app.py
@@ -12,4 +12,9 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+
+@client.event
+async def on_message(message):
+    if 'happy birthday' in message.content.lower():
+        await message.channel.send(f'Happy Birthday! {client.user}')
 client.run(TOKEN)
