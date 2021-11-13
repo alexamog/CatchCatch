708432c5fce487a1e8a5efc82a60a94a5b5c94c5
bleulenny <bleulenny@github.com>
2021-11-12 23:27:11 -0800

    Added more code for roll command

--- app.py
+++ app.py
@@ -20,8 +20,19 @@ db = {}
 
 @bot.command(name='roll')
 async def roll_dice(ctx):
-    choice = random.choice(list(db.keys()))
-    await ctx.channel.send(f'{ctx.author}, you got {choice}!')
+    roll_done = False
+    tries = 0
+    while roll_done == False and tries < len(db.keys()):
+        random_character = random.choice(list(db.keys()))
+        tries += 1
+        if db[random_character].owned == False:
+            db[random_character].owner = ctx.author.id
+            await ctx.channel.send(f'{ctx.author.mention}, you got {random_character}!')
+            roll_done = True
+            return
+        else:
+            tries += 1
+    await ctx.channel.send(f'It seems all characters have been claimed D:')
 
 
 @bot.command(name='create')
@@ -29,6 +40,13 @@ async def roll_dice(ctx):
 async def create(ctx, name, value):
     db[name] = Character(name, value)
     await ctx.channel.send(f'Character name: {name} Character value: {value}')
+
+
+@bot.command(name='info')
+async def create(ctx, character_name):
+    if character_name not in db.keys():
+        await ctx.channel.send(f'Character: {character_name} not in database.')
+    await ctx.channel.send(f'Values: {db[character_name]}')
 
 
 @bot.command(name='trade')
@@ -40,6 +58,5 @@ async def trade(ctx, target: discord.Member):
 async def get(ctx):
     for character, worth in db.items():
         await ctx.channel.send(f'{character}, {worth}')
-
 
 bot.run(TOKEN)
