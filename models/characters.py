4255862b64060b93649b329dd7e6be5caba9b749
bleulenny <bleulenny@github.com>
2021-11-12 22:43:01 -0800

    Restructured code and made getter and setter

--- models/characters.py
+++ models/characters.py
@@ -1,8 +1,24 @@
 class Character:
-    def __init__(self, name, value, owned=False):
+    def __init__(self, name, value, owned=False, owner=None):
         self.name = name
         self.value = value
         self.owned = owned
+        self.owner = owner
 
-    def __owned__(self):
+    def owned(self):
         return self.owned
+
+    def __str__(self):
+        return self.value
+
+    @property
+    def owner(self):
+        """Returns the value of the owner"""
+        return self.owner
+
+    @owner.setter
+    def owner(self, owner):
+        """Sets the owner value"""
+        if self.owned == False and self.owner == None:
+            self.owner = owner
+            self.owned = True
