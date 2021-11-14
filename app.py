2f558665795b83f9ea570d5fb1f7bd3e33c7ac3b
bleulenny <bleulenny@github.com>
2021-11-14 01:02:19 -0800

    Made some improvements for gitignore

--- app.py
+++ app.py
@@ -15,18 +15,19 @@ db = {}  # Temporary
 users = []  # temprary
 
 
-
 @bot.command()
-async def load(ctx,extension): #This loads all the extensions
+async def load(ctx, extension):  # This loads all the extensions
     bot.load_extension(f'cogs.{extension}')
 
+
 @bot.command()
-async def unload(ctx,extension): #This unpacks/unloads the extensions
+async def unload(ctx, extension):  # This unpacks/unloads the extensions
     bot.unload_extension(f'cogs.{extension}')
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
-        bot.load_extension(f'cogs.{filename[:-3]}') #We take out the last 3 characters (.py) since we already specify files that end with .py
+        # We take out the last 3 characters (.py) since we already specify files that end with .py
+        bot.load_extension(f'cogs.{filename[:-3]}')
 
 
 @bot.command(name='slap')
