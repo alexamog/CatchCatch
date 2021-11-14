349c2e353ca4b648ebba4f7914fd1703858d9e5c
bleulenny <bleulenny@github.com>
2021-11-13 16:45:42 -0800

    Added a scoreboard and mockup user db

--- models/characters.py
+++ models/characters.py
@@ -1,17 +1,21 @@
 class Character:
     def __init__(self, name, value, owned=False, owner=None):
         self.name = name
-        self.value = value
+        self.value = int(value)
         self.owned = owned
         self._owner = owner
 
     def owned(self):
         return self.owned
 
-    def __str__(self):
-        if self._owner == None:
-            return f'Character name: {self.name}\nValue: {self.value}'
-        return f'Character name: {self.name}\nValue: {self.value}\n Owned by: {self._owner}'
+    # def __str__(self):
+    #     if self._owner == None:
+    #         return f'```Character name: {self.name}\nValue: {self.value}```'
+    #     return f'```Character name: {self.name}\nValue: {self.value}\nOwned by: {self._owner}\n```'
+
+    def discard(self):
+        self._owner = None
+        self.owned = False
 
     @property
     def owner(self):
@@ -21,6 +25,6 @@ class Character:
     @owner.setter
     def owner(self, owner):
         """Sets the owner value"""
-        if self.owned == False and self.owner == None:
+        if self.owned == False and self._owner == None:
             self._owner = owner
             self.owned = True
