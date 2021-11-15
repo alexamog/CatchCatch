e79a3ffb2635bc5d2cfa42f96a0855fff0610918
bleulenny <bleulenny@github.com>
2021-11-15 10:30:46 -0800

    Addded readability for json file

--- app.py
+++ app.py
@@ -11,7 +11,9 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
 bot = commands.Bot(command_prefix='!')
+
 db = {}  # Temporary
+
 users = []  # temprary
 
 
@@ -20,9 +22,11 @@ async def on_startup():
     await bot.change_presence(statusactivity=discord.Game("ls is a utility."))
     print('Bot is ready.')
 
+
 @bot.command()
 async def load(ctx, extension):  # This loads all the extensions
     bot.load_extension(f'cogs.{extension}')
+
 
 @bot.command()
 async def unload(ctx, extension):  # This unpacks/unloads the extensions
@@ -32,6 +36,7 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         # We take out the last 3 characters (.py) since we already specify files that end with .py
         bot.load_extension(f'cogs.{filename[:-3]}')
+
 
 @bot.command(name='slap')
 async def trade(ctx, target: discord.Member):
--- cogs/user_commands.py
+++ cogs/user_commands.py
@@ -1,23 +1,40 @@
+import json
 import discord
 import random
 from discord.ext import commands
 db = {}  # Temporary
-users = []  # temprary
+users = {
+    "users": []
+}
+
 
 class UserFunctions(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+    with open('database/user_db.json') as fp:
+        data = json.load(fp)
+        for user in data['users']:
+            print(user['user_ID'])
+            print(user['user_Name'])
+            print(user['characters_owned'])
 
     @commands.command(name='register')
     async def register(self, ctx):
+        role_id = 909238338509217805  # Change this depending on server role
         """Provides the user the Gatcha role"""
         role = discord.utils.get(self.bot.get_guild(
-            ctx.guild.id).roles, id=909238338509217805)
+            ctx.guild.id).roles, id=role_id)
         if role in ctx.author.roles:  # Checks if the user already has the specified role
             return await ctx.channel.send(f'You already have the {role} role.')
         await ctx.author.add_roles(role)
         await ctx.channel.send(f'You now have the {role} role!')
-        return users.append(ctx.author)
+        if int(ctx.author.id) not in users["users"]:
+            await ctx.channel.send(f'You have been added to the db')
+            return users['users'].append({
+                "user_ID": int(ctx.author.id),
+                "user_name": ctx.author,
+                "characters_owned": []
+            })
 
     @commands.command(name='roll')
     @commands.has_role('Gatcha')
@@ -66,6 +83,7 @@ class UserFunctions(commands.Cog):
         await ctx.channel.send(f'```Name: {db[character_name].name}\nValue: {db[character_name].value}\nOwned: {db[character_name].owned} ```')
         if discord.File(f'photo_db/{character_name.lower()}.jpg'):
             await ctx.channel.send(file=discord.File(f'photo_db/{character_name.lower()}.jpg'))
+
 
 def setup(bot):
     bot.add_cog(UserFunctions(bot))
