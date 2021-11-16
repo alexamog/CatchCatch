6a050bea0052a985bf130e4b662d7c7714c99e5c
bleulenny <bleulenny@github.com>
2021-11-15 20:26:17 -0800

    Removed uneeded imports from app.py

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
 import os
 import discord
-import random
 from dotenv import load_dotenv
 from discord.ext import commands
 from discord.utils import get
 
-from models import Character
-import requests
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
 bot = commands.Bot(command_prefix='!')
 
+
 @bot.command()
 async def load(ctx, extension):  # This loads all the extensions
     bot.load_extension(f'cogs.{extension}')
+
 
 @bot.command()
 async def unload(ctx, extension):  # This unpacks/unloads the extensions
@@ -24,6 +23,7 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         # We take out the last 3 characters (.py) since we already specify files that end with .py
         bot.load_extension(f'cogs.{filename[:-3]}')
+
 
 @bot.command(name='slap')
 async def trade(ctx, target: discord.Member):
