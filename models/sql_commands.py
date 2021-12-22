8ad366b9dfbd3091025e90842d724b07f86d0bee
Alexander Amog <alex.amog@hotmail.com>
2021-12-22 12:00:40 -0800

    Added code templates for SQL queries

---
+++ models/sql_commands.py
@@ -0,0 +1,23 @@
+def add_chracter(char_name,char_value):
+    """
+    This function adds new characters to the database
+    """
+    pass
+
+def remove_character(char_name):
+    """
+    This function removes a specific character from the database
+    """
+    pass
+
+def get_list(discord_user_id):
+    """
+    This function provides a list of characters depending on the user's id
+    """
+    pass
+
+def leaderboards():
+    """
+    Provides a leaderboards based off score.
+    """
+    pass
--- sqlite.py
+++ sqlite.py
@@ -1,3 +1,12 @@
 import sqlite3
 
-conn = sqlite3.connect("")
+conn = sqlite3.connect("characters.db")
+c = conn.cursor()
+
+c.execute("""INSERT INTO characters VALUES (Frankun, 100, False, 'None') """)
+conn.commit()
+
+# test = c.fetchall()
+# print(test)
+
+conn.close()
