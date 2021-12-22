d90a2aa3c528558bab2aa8102e411e2635fa817b
James Paolo Rili <sbgum@bro.local>
2021-12-22 13:40:56 -0800

    updated owner attributes so it operates on lists

--- models/characters.py
+++ models/characters.py
@@ -1,19 +1,25 @@
+import discord
+
 class Character:
-    def __init__(self, name, value, owned=False, owner=None, times_owned = 0):
+    def __init__(self, name, value, owners = [], times_owned = []):
         self.name = name
         self.value = int(value)
-        self.owned = owned
-        self._owner = owner
-        self.times_owned = times_owned
-
-    def owned(self):
-        return self.owned
-
-    def discard(self):
-        self._owner = None
-        self.owned = False
+        self._owners = owners #list of owners
+        self.times_owned = times_owned #list of natural to times owned for each owner
 
     @property
-    def owner(self):
-        """Returns the value of the owner"""
-        return self._owner
+    def owners(self):
+        """Returns the value of the owners"""
+        return self._owners
+
+    def owned(self):
+        discord.User.id in owners(self)
+
+    def discard(self):
+        uid = discord.User.id
+        ownlist = owners(self)
+        if uid not in ownlist:
+            print('You do not own this character.')
+        else:
+             self._owners = remove(ownlist[index(uid)])
+             self.times_owned = remove(self.times_owned[index(uid)])
