e4ab62348df23a9072cac8fea7fb1cc573ab7e74
Alexander Amog <alex.amog@hotmail.com>
2021-12-22 12:28:05 -0800

    Added add and delete functionality for the dataabse

--- models/sql_commands.py
+++ models/sql_commands.py
@@ -1,23 +1,38 @@
+import sqlite3
+
+conn = sqlite3.connect("characters.db")
+c = conn.cursor()
+
 def add_chracter(char_name,char_value):
     """
     This function adds new characters to the database
     """
-    pass
+
+    c.execute("INSERT INTO characters VALUES (?, ?, ?, ?) ",(char_name,char_value,False,None))
+    conn.commit()
+    print(c.lastrowid)
+    conn.close()
 
 def remove_character(char_name):
     """
     This function removes a specific character from the database
     """
-    pass
+    c.execute("DELETE FROM characters WHERE char_name=char_name", {'char_name':char_name})
+    conn.commit()
+    # print(c.lastrowid)
+    conn.close()
 
 def get_list(discord_user_id):
     """
     This function provides a list of characters depending on the user's id
     """
-    pass
+    
 
 def leaderboards():
     """
     Provides a leaderboards based off score.
     """
     pass
+
+
+# remove_character('TestMe')
