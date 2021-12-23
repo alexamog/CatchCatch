47569bfd2e52563e4c4c7e9e881f5a70689d8df0
James Paolo Rili <sbgum@bro.local>
2021-12-23 11:48:52 -0800

    new file for banners

---
+++ models/banners.py
@@ -0,0 +1,5 @@
+# - Banner is an object that contains 1 rare character, 3 semi rare characters, and common characters.
+# Attributes:
+#   - list of rare characters
+#   - list of semi rare characters
+#   - list of common characters
