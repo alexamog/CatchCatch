ee4051e633ca1fa1fab54aac556c63a0038453ce
James Paolo Rili <sbgum@bro.local>
2021-12-23 12:11:17 -0800

    added semi and rare guarantee probabilities

--- models/banners.py
+++ models/banners.py
@@ -3,3 +3,18 @@
 #   - list of rare characters
 #   - list of semi rare characters
 #   - list of common characters
+
+class Banner:
+    def __init__(self, rare, semi, com):
+        self.rare = rare
+        self.semi = semi
+        self.com = com
+        self.chances = [1, 5, 94]
+
+    def semi_guarantee(self):
+        self.chances = [self.chances[0],
+                        self.chances[1] + self.chances[2],
+                        0]
+    
+    def rare_guarantee(self):
+        self.chances = [1, 0, 0]
